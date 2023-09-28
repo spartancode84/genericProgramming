143ba5793299189aae37354061c2bfe0d8df4362
Oscar Alfredo Chafloque Tampeck <oscar.chafloque@soytul.com>
2023-09-28 17:03:38 -0500

    cambios iniciales

---
+++ src/main/java/com/oasi/genericprogramming/GenericProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.oasi.genericprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenericProgrammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oasi/genericprogramming/GenericProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasi.genericprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
